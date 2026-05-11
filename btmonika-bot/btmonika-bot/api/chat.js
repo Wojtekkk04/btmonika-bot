0bcdc1e40cab6e1f97fb7b22ceef1e90fded8a70
Wojtekkk04 <wkarpinski2204@gmail.com>
2026-05-11 23:05:09 +0200

    Update chat.js

--- btmonika-bot/btmonika-bot/api/chat.js
+++ btmonika-bot/btmonika-bot/api/chat.js
@@ -23,7 +23,7 @@ module.exports = async function handler(req, res) {
 
   try {
     var response = await fetch(
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=' + key,
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=' + key,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
